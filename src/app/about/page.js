a5c9ae1b3c462fbc40614097a86d994304827b23
vardhan1831 <sreevardhan259@gmail.com>
2024-01-26 19:11:12 +0530

    dynamic routes using params and Link

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,5 +1,27 @@
-function AboutPage(){
-    return <h1>About</h1>
-  }
-  
-  export default AboutPage
+import { Fragment } from "react";
+import Link from "next/link";
+
+const details = [
+  { id: 1, name: "Yash", role: "Senior Developer" },
+  { id: 2, name: "Vaibhav", role: "Backend Developer" },
+  { id: 3, name: "Suresh", role: "Frontend Developer" },
+];
+function AboutPage() {
+  return (
+    <Fragment>
+      <ul>
+        <li>
+          <Link href={`about/${details[0].id}`}>{details[0].name}</Link>
+        </li>
+        <li>
+          <Link href={`about/${details[1].id}`}>{details[1].name}</Link>
+        </li>
+        <li>
+          <Link href={`about/${details[2].id}`}>{details[2].name}</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
+}
+
+export default AboutPage;
